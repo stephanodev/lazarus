stephanodev/lazarus

commit edbbc93ae473e6291563b28a2631f3913ada98a2

debian package lazarus: can now compile a gtk2 version
git-svn-id: http://svn2.freepascal.org/svn/lazarus/trunk@11474 4005530d-fff6-0310-9dd1-cebe43e6787f

tools/svn2revisioninc.pas
@@ -60,6 +60,7 @@ type
     RevisionStr: string;
     ConstName: string;
     Verbose: boolean;
+    UseStdOut: boolean;
 
     function FindRevision: boolean;
     function IsValidRevisionInc: boolean;
@@ -109,7 +110,7 @@ var
           Result:=true;
           Show('Retrieved revision with svnversion.');
           Show('');
-          Show('svnversion output:');
+          Show('svnversion error:');
           Show(Copy(Buffer, 1, n));
           Show('');
         except
@@ -246,6 +247,7 @@ begin
   writeln('Options:');
   writeln(' --o                  Output file');
   writeln(' --c=<name>           Name of constant (default RevisionStr)');
+  writeln(' --s                  write revision to stdout, do not create inc file');
   writeln(' --v                  Be more verbose');
   writeln(' --h                  This help screen');
   writeln;
@@ -292,6 +294,9 @@ begin
   if HasOption('v') then
     Verbose := True;
     
+  if HasOption('s') then
+    UseStdOut := True;
+    
   if HasOption('c') then
     ConstName := GetOptionValue('c');
 
@@ -312,7 +317,7 @@ begin
   end;
   
   RevisionIncDirName:=ExtractFilePath(ExpandFileName(RevisionIncFileName));
-  if not DirectoryExists(RevisionIncDirName) then begin
+  if (not UseStdOut) and (not DirectoryExists(RevisionIncDirName)) then begin
     writeln('Error: Target Directory "', RevisionIncDirName, '" doesn''t exist.');
     exit;
   end;
@@ -352,8 +357,14 @@ begin
 
   if not CanCreateRevisionInc then exit;
   
-  if FindRevision or not IsValidRevisionInc then
-    WriteRevisionInc;
+  if FindRevision then begin
+    if UseStdOut then begin
+      writeln(RevisionStr);
+    end else begin
+      if not IsValidRevisionInc then
+        WriteRevisionInc;
+    end;
+  end;
 end;
 
 begin

tools/install/create_lazarus_deb.sh
@@ -1,20 +1,39 @@
-#!/bin/bash
+#!/usr/bin/env bash
+#
+# Author: Mattias Gaertner
+#
+# Usage: ./create_lazarus_deb.sh [gtk2] [release=svn]
+#
+#   Options:
+#     gtk2           compile IDE and programs for gtk2. gtk1 ppu are built too.
+#     release=svn    use svn revision as .deb release tag
 
 set -x
 set -e
+
+LCLWidgetset=
+if [ "$1" = "gtk2" ]; then
+  LCLWidgetset=gtk2
+  shift
+fi
+
+LazRelease='0'
+if [ "$1" = "release=svn" ]; then
+  LazRelease=$(./get_lazarus_revision.sh)
+  shift
+fi
+
+if [ -n "$1" ]; then
+  echo "Usage: ./create_lazarus_deb.sh [gtk2] [release=svn]"
+  exit
+fi
 
 # get FPC version
 FPCVersion=$(fpc -v | grep 'Compiler version' | sed 's/.*\([0-9]\.[0-9]\.[0-9]\).*/\1/')
 Arch=$(fpc -v | grep 'Compiler version' | sed 's/.*for \([^ ]\+\)$/\1/')
 
-# get date of day
-Year=`date +%y`
-Month=`date +%m`
-Day=`date +%d`
-
-Date=20$Year$Month$Day
+Date=`date +%Y%m%d`
 LazVersion=$(./get_lazarus_version.sh)
-LazRelease='0'
 SrcTGZ=lazarus-$LazVersion-$LazRelease.tar.gz
 CurDir=`pwd`
 TmpDir=/tmp/lazarus$LazVersion
@@ -73,23 +92,32 @@ MAKEOPTS="-Fl/opt/gnome/lib"
 if [ -n "$FPCCfg" ]; then
   MAKEOPTS="$MAKEOPTS -n @$FPCCfg"
 fi
+# build for default platform
+make lcl packager/registration ideintf bigidecomponents OPT="$MAKEOPTS"
+# build gtk2 .ppu
+export LCL_PLATFORM=gtk2
+make lcl packager/registration ideintf bigidecomponents OPT="$MAKEOPTS"
+export LCL_PLATFORM=
+# build IDE
+export LCL_PLATFORM=$LCLWidgetset
 make bigide OPT="$MAKEOPTS" USESVN2REVISIONINC=0
 make lazbuilder OPT="$MAKEOPTS"
 make tools OPT="$MAKEOPTS"
-# build gtk2 .ppu
-export LCL_PLATFORM=gtk2
-make lcl ideintf packager/registration bigidecomponents OPT="$MAKEOPTS"
 export LCL_PLATFORM=
+
 strip lazarus
 strip startlazarus
 strip lazbuild
+strip tools/apiwizz/apiwizz
+strip tools/lazres
+strip tools/updatepofiles
 cd -
 
 # create control file
 echo "========================================================================="
 echo "copying control file"
 mkdir -p $LazBuildDir/DEBIAN
-cat $DebianSrcDir/control | \
+cat $DebianSrcDir/control$LCLWidgetset | \
   sed -e "s/FPCVERSION/$FPCVersion/g" \
       -e "s/LAZVERSION/$LazVersion/g" \
       -e "s/ARCH/$Arch/g" \

tools/install/debian_lazarus/controlgtk2
@@ -1,0 +1,13 @@
+Source: lazarus
+Package: lazarus
+Version: LAZVERSION
+Section: devel
+Priority: optional
+Maintainer: Mattias Gaertner <mattias@freepascal.org>
+Architecture: ARCH 
+Depends: libgtk2.0-dev (>=2.6.0), fpc (>=FPCVERSION) | fp-compiler (>=FPCVERSION), fpc-src (>=FPCVERSION), fpc (>=FPCVERSION) | fp-ide (>=FPCVERSION), fpc (>=FPCVERSION) | fp-units-base (>=FPCVERSION), fpc (>=FPCVERSION) | fp-units-db (>=FPCVERSION), fpc (>=FPCVERSION) | fp-units-fcl (>=FPCVERSION), fpc (>=FPCVERSION) | fp-units-fv (>=FPCVERSION), fpc (>=FPCVERSION) | fp-units-gfx (>=FPCVERSION), fpc (>=FPCVERSION) | fp-units-gnome1 (>=FPCVERSION), fpc (>=FPCVERSION) | fp-units-gtk (>=FPCVERSION), fpc (>=FPCVERSION) | fp-units-gtk2 (>=FPCVERSION), fpc (>=FPCVERSION) | fp-units-misc (>=FPCVERSION), fpc (>=FPCVERSION) | fp-units-net (>=FPCVERSION), fpc (>=FPCVERSION) | fp-units-rtl (>=FPCVERSION), fpc (>=FPCVERSION) | fp-utils (>=FPCVERSION)
+Description: RAD tool for freepascal
+  Lazarus is a free and opensource IDE and RAD tool for Free Pascal using the
+  Lazarus component library LCL. The LCL is included in this package too.
+  See http://www.lazarus.freepascal.org
+  

tools/install/get_lazarus_revision.sh
@@ -1,0 +1,11 @@
+#!/usr/bin/env bash
+
+if [ ! -f ../svn2revisioninc ]; then
+  make -C ../../lcl
+  make -C ..
+fi
+
+../svn2revisioninc -s
+
+# end.
+

tools/lazdde/frmgeneratesql.lrs
@@ -1,5 +1,3 @@
-{ This is an automatically generated lazarus resource file }
-
 LazarusResources.Add('TGenerateSQLForm','FORMDATA',[
   'TPF0'#16'TGenerateSQLForm'#15'GenerateSQLForm'#4'Left'#3']'#1#6'Height'#3'e'
   +#1#3'Top'#3#17#1#5'Width'#3'$'#2#18'HorzScrollBar.Page'#3'#'#2#18'VertScroll'

tools/lazdde/frmimportdd.lrs
@@ -1,5 +1,3 @@
-{ This is an automatically generated lazarus resource file }
-
 LazarusResources.Add('TImportDDform','FORMDATA',[
   'TPF0'#13'TImportDDform'#12'ImportDDform'#4'Left'#3'p'#1#6'Height'#3','#1#3'T'
   +'op'#3#23#1#5'Width'#3'q'#1#18'HorzScrollBar.Page'#3'p'#1#18'VertScrollBar.P'

tools/lazdde/frmmain.lrs
@@ -1,5 +1,3 @@
-{ This is an automatically generated lazarus resource file }
-
 LazarusResources.Add('TMainForm','FORMDATA',[
   'TPF0'#9'TMainForm'#8'MainForm'#4'Left'#3#15#3#6'Height'#3'v'#1#3'Top'#3#206#0
   +#5'Width'#3#17#2#18'HorzScrollBar.Page'#3#16#2#18'VertScrollBar.Page'#3'['#1

tools/lazdde/frmsqlconnect.lrs
@@ -1,5 +1,3 @@
-{ This is an automatically generated lazarus resource file }
-
 LazarusResources.Add('TSQLConnectionForm','FORMDATA',[
   'TPF0'#18'TSQLConnectionForm'#17'SQLConnectionForm'#4'Left'#3'%'#2#6'Height'#3
   +#177#0#3'Top'#3#198#1#5'Width'#3':'#1#18'HorzScrollBar.Page'#3'9'#1#18'VertS'

commit 62cbbc74067f41757f574fa85361ee9e1e1eacd6

svn2revisioninc : always write revision.inc, except if there is a good revision.inc and no svn info (got broken in r11474)
git-svn-id: http://svn2.freepascal.org/svn/lazarus/trunk@11827 4005530d-fff6-0310-9dd1-cebe43e6787f

tools/svn2revisioninc.pas
@@ -357,14 +357,12 @@ begin
 
   if not CanCreateRevisionInc then exit;
   
-  if FindRevision then begin
-    if UseStdOut then begin
+  if UseStdOut then begin
+    if FindRevision then
       writeln(RevisionStr);
-    end else begin
-      if not IsValidRevisionInc then
-        WriteRevisionInc;
-    end;
-  end;
+  end
+  else if FindRevision or not IsValidRevisionInc then
+    WriteRevisionInc;
 end;
 
 begin